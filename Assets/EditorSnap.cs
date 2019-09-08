0572f8d613d940deaf861adfd8902cafcd370d4f
dmdylan <dylandanowski@gmail.com>
2019-09-08 18:31:06 -0400

    added block/grid prefab and grid snapping script

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
+}
